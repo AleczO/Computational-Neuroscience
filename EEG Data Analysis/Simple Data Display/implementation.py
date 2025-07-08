26686610ed04adf7654cd7c50adcaf2108073b28
AleczO <alecz0.code@gmail.com>
2025-07-08 19:27:34 +0200

    Update

--- EEG Data Analysis/Simple Data Display/implementation.py
+++ EEG Data Analysis/Simple Data Display/implementation.py
@@ -4,4 +4,5 @@ import mne
 
 file = mne.io.read_raw_eeglab("EEG Data Analysis/Simple Data Display/002.set") 
 
-file.plot(start=0, duration=5, block=True)
+file.compute_psd(fmax=50).plot(picks="data", exclude="bads", amplitude=False)
+file.plot(duration=5, n_channels=2, block=True)
